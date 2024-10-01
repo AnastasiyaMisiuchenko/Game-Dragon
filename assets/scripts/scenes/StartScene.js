e5ffc4816db45b8563a03dc963515d4fb88c2bfb
NastenokKotenok <classisintern@gmail.com>
2024-10-01 12:59:25 +0300

    Initial commit

---
+++ assets/scripts/scenes/StartScene.js
@@ -0,0 +1,29 @@
+class StartScene extends Phaser.Scene {
+    constructor() {
+        super('Start');
+    }
+
+    create() {
+        this.createBackground();
+        this.createText();
+        this.setEvents();
+    }
+
+    createBackground(){
+        this.add.sprite(0, 0, 'bg').setOrigin(0);
+    }
+
+    createText(){
+        this.add.text(config.width / 2, 300, 'Tap to start',{
+            font: '40px CurseCasual',
+            fill: '#FFFFFF'
+        }).setOrigin(0.5)
+    }
+
+    setEvents(){
+        this.input.on('pointerdown', () => {
+            this.scene.start('Game');
+        })
+    }
+
+}
